0aa82e9499f07f735b8553b04e76c44309cdbb6f
Carlos Eduardo Tabare Camel <147793216+NickolaiGaming@users.noreply.github.com>
2024-01-16 15:26:25 +0000

    falta la parte de john

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import NavBar from './Components/navbar';
+import Jumbotron from './Components/jumbotron';
+
+
 
 const App = () => {
     return (
-        <h1>REACT APP</h1>
+        <>
+        <NavBar titulo="Start Bootstrap"/>
+        <div className='container-fluid mt-4'>
+        <Jumbotron 
+        exampleButon = "Titulo Boton"
+        h1="A Warm Welcome!" 
+        description="Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking."
+        />
+        </div>
+        </>
+        
     )
 }
 
